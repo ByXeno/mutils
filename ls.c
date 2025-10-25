0a990bcda9a090c18c214a6e31a794fa633b90fd
Menderes Sabaz <sabazmenderes@gmail.com>
2025-10-25 13:41:26 +0300

    Sorted

--- ls.c
+++ ls.c
@@ -1,61 +1,19 @@
 
+#define _DEFAULT_SOURCE
 #include "common.h"
 #include <dirent.h>
 
-char cast_to_lower(char c)
-{
-    if('A' <= c && c <= 'Z')
-    {return c+32;}
-    return c;
-}
-
-char** compare_strings(char** item, char** stack, uint32_t j) {
-    char *str1 = *item;
-    char *str2 = stack[j];
-    uint32_t k = 0;
-    while (1) {
-        char c1 = cast_to_lower(str1[k]);
-        char c2 = cast_to_lower(str2[k]);
-        if (c1 == '\0' && c2 == '\0') return item;
-        if (c1 == '\0') return item;
-        if (c2 == '\0') return &stack[j];
-        if (c1 > c2) return &stack[j];
-        if (c1 < c2) return item;
-        k++;
-    }
-}
-
 bool ls(char* path)
 {
-    DIR* d = opendir(path);
-    if(!d) return false;
-    struct dirent* ent = 0;
-    uint32_t count = 0;
-    while((ent = readdir(d)) != NULL)
-    {count++;}
-    rewinddir(d);
-    char* stack[count*sizeof(char*)];
-    char** ptr = stack; 
-    while((ent = readdir(d)) != NULL)
-    {*ptr = ent->d_name;ptr++;}
-    uint32_t i = 0;
-    for(;i < count;++i)
-    {
-        uint32_t j = 0;
-        char** item = 0;
-        for(;j < count;++j)
-        {
-            if(!(stack[j])) continue;
-            if(!item)
-            {item = &stack[j];continue;}
-            item = compare_strings(item,stack,j);
-        }
-        printf("%s\n",*item);
-        *item = 0;
+    struct dirent **namelist;
+    int n = scandir(path, &namelist, NULL, alphasort);
+    if (n < 0) return false;
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", namelist[i]->d_name);
+        free(namelist[i]);
     }
-    
-    closedir(d);
-    return (errno != 0);
+    free(namelist);
+    return true;
 }
 
 int main(int argc,char** argv)
